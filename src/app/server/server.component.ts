4715c8db51cbbbd6ba41dc4b17cc682b27d84790
Matheus-Franklin10 <matheusjfranklin@gmail.com>
2022-07-25 14:43:09 -0300

    Angular course progress

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent{
+    serverID: number = 10;
+    serverStatus: string = 'offline';
+    
+    getServerId(){
+        return this.serverID;
+    }
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+}
